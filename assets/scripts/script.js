6e4409a63239d48cd3abf4c16b39ee2e9b123842
Leah Garrison <lgarrison1023@gmail.com>
2021-08-14 09:53:55 -0400

    added script.js

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const words = ["ingests","transforms", "syncs"];
+let i = 0;
+
+const _changeText = () => {
+  i = (i + 1) % words.length;
+  document.getElementById("spin").innerHTML = words[i];
+}
+
+setInterval("_changeText()", 800);
